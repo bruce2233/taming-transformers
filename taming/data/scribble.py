8268d9c1cbf3991bda0689d5ab1ada6463cdf684
bruce2233 <864427717@qq.com>
2023-10-11 07:28:39 +0000

    from pretrained

--- taming/data/scribble.py
+++ taming/data/scribble.py
@@ -2,7 +2,7 @@ from controlnet_aux import HEDdetector
 
 class ScribblePreprocessor:
     def __init__(self):
-        hed = HEDdetector.from_pretrained("~/.cache/huggingface/hub/models--lllyasviel--Annotators/snapshots/982e7edaec38759d914a963c48c4726685de7d9/")
+        hed = HEDdetector.from_pretrained("/home/bruce/.cache/huggingface/hub/models--lllyasviel--Annotators/blobs/",filename="5ca93762ffd68a29fee1af9d495bf6aab80ae86f08905fb35472a083a4c7a8fa")
         self.hed = hed
         
     def image_scribble(self, image):
